db5e1c0534611de7774bcd70924265a8f1bf5467
Abouzaid Tarek <if18b119@technikum-wien.at>
2021-04-05 14:25:06 +0200

    Started over the project, added the ViewModelBase and RelayCommand class

---
+++ TourPlaner/ViewModels/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace TourPlaner.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Wenn sich ein property mit einem gewisse namen ändert wird ein event geschossen um die ui zu notifizieren
+        protected void RaisePropertyChangedEvent([CallerMemberName] string propertyName = "")
+        {
+            ValidatePropertyName(propertyName);
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        //ist ein validator um zu überprüfen ob es den namen überhaupt gibt
+        protected void ValidatePropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                throw new ArgumentException("Invalid property name: " + propertyName);
+            }
+        }
+    }
+}
